484bf4b52fb8a1025cc40488ff2bd6aeec8496fb
anhkiet210 <anhkietk053@gmail.com>
2022-09-23 20:56:02 +0700

    add link for menu

--- src/layouts/Layout/components/Header/MenuBar/index.jsx
+++ src/layouts/Layout/components/Header/MenuBar/index.jsx
@@ -1,34 +1,44 @@
-import { Col, Row, Menu, Button } from "antd";
-import Affix from "antd/lib/affix";
-import classNames from "classnames/bind";
-import style from './MenuBar.module.css'
+import { Col, Row, Menu, Button } from 'antd';
+import Affix from 'antd/lib/affix';
+import classNames from 'classnames/bind';
+import { Link, NavLink } from 'react-router-dom';
+import style from './MenuBar.module.css';
 
-const cx = classNames.bind(style)
+const cx = classNames.bind(style);
+
+const menu = [
+    {
+        id: 1,
+        name: 'Home',
+        link: '/',
+    },
+    {
+        id: 2,
+        name: 'My account',
+        link: '/my-account',
+    },
+];
 
 function MenuBar() {
     return (
         <>
             <Affix offsetTop={0}>
-                <Row justify="center" style={{backgroundColor: '#fff'}}>
+                <Row justify="center" style={{ backgroundColor: '#fff' }}>
                     <Col span={18}>
                         <Row className={cx('menu-bar')}>
                             <Col md={16}>
                                 <Menu mode="horizontal" className={cx('menu')}>
-                                    <Menu.Item key="mail" className={cx('menu-item')}>
-                                        Navigation One
-                                    </Menu.Item>
-                                    <Menu.SubMenu key="SubMenu" title="Navigation Two - Submenu" className={cx('menu-item')}>
-                                        <Menu.Item key="two" className={cx('menu-item')} >
-                                            Navigation Two
+                                    {menu.map((item) => (
+                                        <Menu.Item className={cx('menu-item')} key={item.id}>
+                                            <NavLink to={item.link}>{item.name}</NavLink>
                                         </Menu.Item>
-                                        <Menu.Item key="three" className={cx('menu-item')} >
-                                            Navigation Three
-                                        </Menu.Item>
-                                    </Menu.SubMenu>
+                                    ))}
                                 </Menu>
                             </Col>
-                            <Col md={8} className={cx('wrap-btn')} >
-                                <Button className={cx('btn-appoitment')}>Make an Appointment</Button>
+                            <Col md={8} className={cx('wrap-btn')}>
+                                <Link to="/appointment" className={cx('btn-appoitment')}>
+                                    Make an Appointment
+                                </Link>
                             </Col>
                         </Row>
                     </Col>
